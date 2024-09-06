8257b82596ec7a2d0408c546f0704a4df7d028a8
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-09-05 21:58:33 -0400

    Finished setting up server and a boiler page.

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 import express from "express";
 
-const app = exress();
+const PORT = 5988;
+const app = express();
+
+// Start: npm run dev
+// Stop: Hold Ctrl and then C
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("VIRTUAL PIANO MAPPER");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
