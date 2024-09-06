2d4a30b6a5bfa4c13d0a952968600776eafd7c1e
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-09-05 23:06:28 -0400

    Linked up MongoDB to backend.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 5988;
+const MONGO_DB_URL = "mongodb+srv://root:rootpass@song-notes-db.lp1nq.mongodb.net/?retryWrites=true&w=majority&appName=Song-Notes-DB"
 const app = express();
 
 // Start: npm run dev
@@ -11,6 +13,12 @@ app.get('/', (request, response) => {
     return response.status(234).send("VIRTUAL PIANO MAPPER");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connect(MONGO_DB_URL).then(() => {
+    console.log("connected to DB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
+
+})
