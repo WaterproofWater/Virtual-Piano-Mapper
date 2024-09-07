547a7ec5732f0a7d5ccf2c607defe0f28aabb360
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-09-07 16:11:42 -0400

    Initialized routes for create, delete, edit, and detail functionalities for the songs.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import CreateSong from "./pages/CreateSong.jsx"
+import ShowSong from "./pages/ShowSong.jsx"
+import EditSong from "./pages/EditSong.jsx"
+import DeleteSong from "./pages/DeleteSong.jsx"
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white"> App </div>
+    <Routes> 
+      <Route path = "/" element={<Home />} /> 
+      <Route path = "/songs/create" element={<CreateSong />} /> 
+      <Route path = "/songs/details/:id" element={<ShowSong />} /> 
+      <Route path = "/songs/edit/:id" element={<EditSong />} /> 
+      <Route path = "/songs/delete/:id" element={<DeleteSong />} /> 
+    </Routes>
   )
 }
 
