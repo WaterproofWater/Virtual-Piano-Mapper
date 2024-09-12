4dd01bb2ba05c018b5beb28b191424a9231ddf23
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-09-12 18:45:26 -0400

    Implemented notes mapping at a basic level

--- frontend/mapper/NotesMapper.jsx
+++ frontend/mapper/NotesMapper.jsx
@@ -1,13 +1,44 @@
 const NotesMapper = (data) => {
     const { notes, keyMap } = data;
-  
-    console.log(notes);
-    console.log(keyMap);
-  
-    let script = "testeeeees";
-  
+
+    let script = ""; 
+    let waitTime = 0;  
+
+    const lines = notes.split('\n');
+
+    for (let line of lines) {
+        const [octave, notesInLine] = line.split('|');
+        if (!notesInLine) {
+            console.log('No notes found for this line');
+            continue;
+        }
+
+        for (let i = 0; i < notesInLine.length; i++) {
+            const note = notesInLine[i];
+
+            if (note === '-') {
+                waitTime += 167;
+            } 
+            else if (note !== ' ') {
+                const noteKey = `${note}${octave}`;
+                const mappedKey = keyMap[noteKey];
+
+                if (mappedKey) {
+                    if (waitTime > 0) {
+                        script += `sleep, ${waitTime}\n`;
+                    }
+                    script += `send, ${mappedKey}\n`;
+                    waitTime = 0;
+                } else {
+                    console.log(`Note ${noteKey} not found in keyMap`);
+                }
+            }
+        }
+    }
+
+    console.log(script);
+
     return script;
-  };
-  
-  export default NotesMapper;
-  
+};
+
+export default NotesMapper;
